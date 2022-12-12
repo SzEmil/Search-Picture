ff492aba6da1982295eaeb6a07f207eaf5d305d8
Emil Szymczyk <106548124+SzEmil@users.noreply.github.com>
2022-12-12 20:26:26 +0100

    lighbox bug fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import Notiflix from 'notiflix';
-import SimpleLightbox from 'simplelightbox/dist/simple-lightbox.esm';
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('#search-form');
